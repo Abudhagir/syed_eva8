45963ef8934e63807058e0040c1cde82688a01eb
Abudhagir <syedabudhagir@gmail.com>
2023-02-24 21:53:20 +0530

    Update train.py

--- utils/train.py
+++ utils/train.py
@@ -1,3 +1,27 @@
+from ignite.engine import create_supervised_trainer, create_supervised_evaluator
+from ignite.metrics import Loss, Accuracy
+from ignite.contrib.handlers import FastaiLRFinder, ProgressBar
+
+def range_test(model, optimizer, criterion, device, trainloader, testloader, start_lr = 1e-4, end_lr = 10, max_epochs = 10):
+    trainer = create_supervised_trainer(model, optimizer, criterion, device=device)
+    ProgressBar(persist=True).attach(trainer, output_transform=lambda x: {"batch loss": x})
+
+    lr_finder = FastaiLRFinder()
+    to_save={'model': model, 'optimizer': optimizer}
+    with lr_finder.attach(trainer, to_save, start_lr = start_lr, end_lr = end_lr, diverge_th=1.5) as trainer_with_lr_finder:
+        trainer_with_lr_finder.run(trainloader)
+        
+    trainer.run(trainloader, max_epochs=10)
+
+    evaluator = create_supervised_evaluator(model, metrics={"acc": Accuracy(), "loss": Loss(criterion)}, device=device)
+    evaluator.run(testloader)
+
+    print(evaluator.state.metrics)
+
+    return lr_finder
+
+'''
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -66,3 +90,4 @@ def train(model, device, train_loader, train_acc, train_loss, optimizer, schedul
 
         pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} LR={lrs[-1]:0.5f} Accuracy={100*correct/processed:0.2f}')
         train_acc.append(100*correct/processed)
+'''
