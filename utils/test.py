60e0feca01a07828f53d90dcb3c4ea6977d5719e
Abudhagir <syedabudhagir@gmail.com>
2023-02-24 22:52:37 +0530

    Update test.py

--- utils/test.py
+++ utils/test.py
@@ -3,6 +3,119 @@ import torch.nn.functional as F
 import torch.nn as nn
 
 
+def test(model, device, test_loader, criterion):
+    """put model in eval mode and test it
+
+    Args:
+        model (instance): torch model instance of defined model
+        device (str): "cpu" or "cuda" device to be used
+        test_loader (instance): Torch Dataloader instance for testset
+        criterion (instance): criterion to used for calculating the loss
+
+    Returns:
+        float: accuracy and loss values
+    """
+    model.eval()
+    test_loss = 0
+    correct = 0
+    #iteration = len(test_loader.dataset)// test_loader.batch_size
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += criterion(output, target).item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    return 100. * correct / len(test_loader.dataset), test_loss
+
+
+def save_model(model, epoch, optimizer, path):
+    """Save torch model in .pt format
+
+    Args:
+        model (instace): torch instance of model to be saved
+        epoch (int): epoch num
+        optimizer (instance): torch optimizer
+        path (str): model saving path
+    """
+    state = {
+        'epoch': epoch,
+        'state_dict': model.state_dict(),
+        'optimizer': optimizer.state_dict()
+    }
+    torch.save(state, path)
+
+
+def fit_model(net, optimizer, criterion, device, NUM_EPOCHS,train_loader, test_loader, use_l1=False, scheduler=None, save_best=False):
+    """Fit the model
+
+    Args:
+        net (instance): torch model instance of defined model
+        optimizer (function): optimizer to be used
+        criterion (instance): criterion to used for calculating the loss
+        device (str): "cpu" or "cuda" device to be used
+        NUM_EPOCHS (int): number of epochs for model to be trained
+        train_loader (instance): Torch Dataloader instance for trainingset
+        test_loader (instance): Torch Dataloader instance for testset
+        use_l1 (bool, optional): L1 Regularization method set True to use. Defaults to False.
+        scheduler (function, optional): scheduler to be used. Defaults to None.
+        save_best (bool, optional): If save best model to model.pt file, paramater validation loss will be monitered
+
+    Returns:
+        (model, list): trained model and training logs
+    """
+    training_acc, training_loss, testing_acc, testing_loss = list(), list(), list(), list()
+    lr_trend = []
+    if save_best:
+        min_val_loss = np.inf
+        save_path = 'model.pt'
+
+    for epoch in range(1,NUM_EPOCHS+1):
+        print("EPOCH: {} (LR: {})".format(epoch, optimizer.param_groups[0]['lr']))
+        
+        train_acc, train_loss, lr_hist = train(
+            model=net, 
+            device=device, 
+            train_loader=train_loader, 
+            criterion=criterion ,
+            optimizer=optimizer, 
+            use_l1=use_l1, 
+            scheduler=scheduler
+        )
+        test_acc, test_loss = test(net, device, test_loader, criterion)
+        # update LR
+        if scheduler:
+            if isinstance(scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
+                scheduler.step(test_loss)
+        
+        if save_best:
+            if test_loss < min_val_loss:
+                print(f'Valid loss reduced from {min_val_loss:.5f} to {test_loss:.6f}. checkpoint created at...{save_path}\n')
+                save_model(net, epoch, optimizer, save_path)
+                min_val_loss = test_loss
+            else:
+                print(f'Valid loss did not inprove from {min_val_loss:.5f}\n')
+        else:
+            print()
+
+        training_acc.append(train_acc)
+        training_loss.append(train_loss)
+        testing_acc.append(test_acc)
+        testing_loss.append(test_loss)
+        lr_trend.extend(lr_hist)    
+
+    if scheduler:   
+        return net, (training_acc, training_loss, testing_acc, testing_loss, lr_trend)
+    else:
+        return net, (training_acc, training_loss, testing_acc, testing_loss)
+
+'''
 def test(model, device, test_loader, test_acc, test_losses, scheduler, criterion):
     model.eval()
     test_loss = 0
@@ -28,7 +141,6 @@ def test(model, device, test_loader, test_acc, test_losses, scheduler, criterion
     test_acc.append(100. * correct / len(test_loader.dataset))
 
 
-'''
 
 import torch
 import torch.nn.functional as F
